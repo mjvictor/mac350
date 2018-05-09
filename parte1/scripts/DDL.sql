3d20ad8c682f619a5505875000b865f0e6d64a03
Victor João <victor.mjoao@gmail.com>
2018-05-09 19:11:18 -0300

    Create and update sql scripts
    Create SERVICO, TRILHA, PERFIL, MODULO, USUARIO tables.
    Create a simple query for each table.
    Create script that cleans all table content.
    Create script that insert into the tables some content.
    Create script that drop all tables.
    
    Signed-off-by: Victor João <victor.mjoao@gmail.com>

--- parte1/scripts/DDL.sql
+++ parte1/scripts/DDL.sql
@@ -1,5 +1,45 @@
-CREATE TABLE figurinhas (
-    nome     character(20),
-    numero   integer,
-    tipo     character
+CREATE TABLE SERVICO(
+    cod_ser 	integer not NULL,
+    nome_ser 	varchar(50) not NULL,
+    CONSTRAINT pk_ser PRIMARY KEY (cod_ser)
 );
+
+CREATE TABLE TRILHA(
+    cod_tr 	integer not NULL,
+    nome_tr	varchar(50),
+    CONSTRAINT pk_tr PRIMARY KEY (cod_tr)
+);
+
+CREATE TABLE PERFIL(
+    cod_per	integer not NULL,
+    nome_per	varchar(50) not NULL,
+    CONSTRAINT pk_per PRIMARY KEY (cod_per)
+);
+
+CREATE TABLE MODULO(
+    cod_mod	integer not NULL,
+    nome_mod	varchar(50) not NULL,
+    min_dis	integer not NULL,
+    total_dis	integer not NULL,
+    cod_tr 	integer not NULL,
+    CONSTRAINT pk_mod PRIMARY KEY (cod_mod),
+    CONSTRAINT fk_tr 
+	FOREIGN KEY (cod_tr)
+	REFERENCES TRILHA(cod_tr)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+CREATE TABLE USUARIO(
+    login	varchar(100) not NULL,
+    senha	varchar(6) not NULL,
+    nome	varchar(50) not NULL,
+    cod_per	integer not NULL,
+    CONSTRAINT pk_us PRIMARY KEY (login),
+    CONSTRAINT fk_per
+	FOREIGN KEY (cod_per)
+	REFERENCES PERFIL(cod_per)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);	
+
---
+++ parte1/scripts/DML_INSERT.sql
@@ -0,0 +1,30 @@
+INSERT INTO SERVICO
+VALUES(1, 'Inserção'),
+      (2, 'Vizualização'),
+      (3, 'Alteração'),
+      (4, 'Remoção');
+
+INSERT INTO TRILHA
+VALUES(1, 'Sistemas'),
+      (2, 'IA'),
+      (3, 'Ciência de dados'),
+      (4, 'Teoria da computação');
+
+INSERT INTO PERFIL
+VALUES(1, 'Comum'),
+      (2, 'Superusuário');
+
+INSERT INTO MODULO
+VALUES(1, 'Desenvolvimento de software', 4, 4, 1),
+      (2, 'Banco de Dados', 2, 3, 1),
+      (3, 'Introdução a IA', 2, 3, 2),
+      (4, 'Núcleo 1', 5, 5, 3),
+      (5, 'Otimização 1', 2, 2, 4);
+
+INSERT INTO USUARIO
+VALUES('victor.joao@usp.br', 'fea342', 'Victor João', 2),
+      ('leolanavo@gmail.com', 'fae722', 'Leonardo Lana', 1),
+      ('beatriz.marouelli@usp.br','imebcc', 'Beatriz Marouelli', 1),
+      ('akasolak@gmail.com', 'fie828', 'André Solak', 2),
+      ('gabriely.pereira@usp.br', 'fje902', 'Gabriely Rangel', 2);
+
